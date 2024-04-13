b5027951ab24f11a4df5a491460ad7e6c6085b0e
SukhorukovR <166890905+SukhorukovR@users.noreply.github.com>
2024-04-13 09:46:49 +0500

    Update code.js

--- Lab 11/code.js
+++ Lab 11/code.js
@@ -1,12 +1,15 @@
 function Calculation(){
+    // Получаем значения из полей ввода
     var n = document.getElementsByName("number_n")[0].value;
     var m = document.getElementsByName("number_m")[0].value;
 
+    // Проверяем валидность введенных данных
     if(!/^[0-9]+$/.test(n) || !/^[0-9]+$/.test(m) || m < 1 || n < 1){
+        // Выводим сообщение об ошибке, если данные неверные
         alert("Пожалуйста, убедитесь, что вы вводите только натуральные числа во все поля.");
     }else if(n > 0 && m > 0){
+        // Вычисляем НОК и выводим результат
         var multiple = n * m/ gcd(n, m);
-
         document.getElementById("multiple").textContent = "НОК = " + multiple;
     }
 }
