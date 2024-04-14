c0efa65c886a784d782413c851bcc014cd201d50
SukhorukovR <166890905+SukhorukovR@users.noreply.github.com>
2024-04-14 11:02:20 +0500

    Update code.js

--- Lab 11/code.js
+++ Lab 11/code.js
@@ -1,17 +1,11 @@
-function Calculation(){
-    // Получаем значения из полей ввода
-    var n = document.getElementsByName("number_n")[0].value;
-    var m = document.getElementsByName("number_m")[0].value;
+alert("Найти наименьшее общее кратное (НОК) для натуральных значений n и m.");
+var n = prompt("Введите натуральное число n");
+var m = prompt("Введите натуральное число m");
 
-    // Проверяем валидность введенных данных
-    if(!/^[0-9]+$/.test(n) || !/^[0-9]+$/.test(m) || m < 1 || n < 1){
-        // Выводим сообщение об ошибке, если данные неверные
-        alert("Пожалуйста, убедитесь, что вы вводите только натуральные числа во все поля.");
-    }else if(n > 0 && m > 0){
-        // Вычисляем НОК и выводим результат
-        var multiple = n * m/ gcd(n, m);
-        document.getElementById("multiple").textContent = "НОК = " + multiple;
-    }
+if(!/^[0-9]+$/.test(n) || !/^[0-9]+$/.test(m) || m < 1 || n < 1){
+    alert("Пожалуйста, убедитесь, что вы вводите только натуральные числа во все поля при следующем запуске.");
+}else if(n > 0 && m > 0){
+    alert("Результат: " + (n * m/ gcd(n, m)));
 }
 
 function gcd(a, b) {
